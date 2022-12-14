bf67a721d4aa566ad97ec5bed36c3fa2ff1d11f2
Vedant Tarale <vedanttarale10@gmail.com>
2022-12-14 22:22:51 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let element;
 function play_pause_audio() {
-  if ((element = document.getElementsByClassName("fa-play")[0])) {
+  if ((element = document.getElementsByClassName("fa-play fa-3x")[0])) {
     document.getElementById("music").play();
     element.setAttribute("class", "fa fa-pause fa-3x");
   } else {
-    element = document.getElementsByClassName("fa-pause")[0];
+    element = document.getElementsByClassName("fa-pause fa-3x")[0];
     document.getElementById("music").pause();
     element.setAttribute("class", "fa fa-play fa-3x");
   }
